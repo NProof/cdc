5e14824230168b3406259c54930bf9329288d573
NProof <34260623+NProof@users.noreply.github.com>
2018-10-17 10:59:52 +0800

    part dark in nextMov.

--- Board.cpp
+++ Board.cpp
@@ -26,9 +26,30 @@ void Board::eati(Mov mov)
 	cheses[mov.src].type = 0;
 }
 
+int index32[32] = {31,0,1,5,2,16,27,6,3,14,17,19,28,11,7,21,30,4,15,26,13,18,10,20,29,25,12,9,24,8,23,22};
+
+int BitsHash(__UINT32_TYPE__ x)
+{
+	return (x * 0x08ED2BE6) >> 27;
+}
+
+int GetIndex(__UINT32_TYPE__ mask)
+{
+	return index32[BitsHash(mask)];
+}
+
 vector<Mov *> Board::nextMovs()
 {
-	return vector<Mov *>();
+    vector<Mov *> ret;
+    int src;
+    __UINT32_TYPE__ pos = darks;
+    while(pos){
+        __UINT32_TYPE__ mask = pos & (-pos);
+        pos ^= mask;
+        src = GetIndex(mask);
+        ret.push_back(new Mov(1,src,15));
+    }
+	return ret;
 }
 
 ostream & operator<<(ostream & os, const Board board)
