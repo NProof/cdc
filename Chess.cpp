4db20208cbdfa7449d368b76d055f363bd32ca7a
NProof <34260623+NProof@users.noreply.github.com>
2018-10-17 12:21:08 +0800

    isC and canMov

--- Chess.cpp
+++ Chess.cpp
@@ -10,6 +10,51 @@ Chess::~Chess()
 {
 	//dtor
 }
+bool Chess::isC()
+{
+    return type==6||type==13;
+}
+
+bool Chess::canMov(int betype)
+{
+    switch(type)
+    {
+    case 0: //  "-";
+        return false;
+	case 1: // "k";
+		return 7<betype&&betype<14;
+	case 2: // "g";
+		return 8<betype&&betype<15;
+	case 3: // "m";
+		return 9<betype&&betype<15;
+	case 4: // "r";
+		return 10<betype&&betype<15;
+	case 5: // "n";
+		return 11<betype&&betype<15;
+	case 6: // "c";
+		exit(2);
+	case 7: // "p";
+		return betype==8||betype==14;
+	case 8: // "K";
+		return 0<betype&&betype<7;
+	case 9: // "G";
+		return 1<betype&&betype<8;
+	case 10: // "M";
+		return 2<betype&&betype<8;
+	case 11: // "R";
+		return 3<betype&&betype<8;
+	case 12: // "N";
+		return 4<betype&&betype<8;
+	case 13: // "C";
+        exit(2);
+	case 14: // 'P'
+	    betype==7;
+	case 15: // 'x'
+        exit(3);
+	default: // others.
+		exit(1);
+    };
+}
 
 ostream & operator<<(ostream & os, const Chess chess)
 {
